b3ad3dd1b9cb9cbc7f20ead2aa192d2e53107df2
pad <pad@facebook.com>
2010-11-09 20:42:47 -0800

    syncwebize arch/dummy/ but depends on camlburg and the x86 target

---
+++ arch/dummy/dummy.mli
@@ -0,0 +1,16 @@
+(*s: dummy.mli *)
+module type ARCH = sig
+    val arch:           string (* name of this architecture *)
+    val byte_order:     Rtl.aggregation
+    val wordsize:       int
+    val pointersize:    int
+    val memsize:        int
+end
+
+module Make(A: ARCH): sig
+    val target' :       Ast2ir.tgt
+end    
+(*x: dummy.mli *)
+val dummy32l':  Ast2ir.tgt
+val dummy32b':  Ast2ir.tgt
+(*e: dummy.mli *)
